09d2ca42f91f17b3d98268500fdc718d2ecb29c4
Chase Davis <chase.davis@gmail.com>
2013-01-10 16:03:01 -0800

    Starting assignment step cleanup

--- campfin/bin/assign.py
+++ campfin/bin/assign.py
@@ -1,15 +1,23 @@
 import string
 import networkx as nx
-from apps.fec.models import Match, Individual, Contribution
+from apps.fec.models import Match, Contribution
 from networkx.algorithms.components.connected import connected_components
 from sklearn.tree import DecisionTreeClassifier
 
 training = Match.objects.filter(for_training=True)
 training_correct = [int(t.same) for t in training]
 
-for letter in string.uppercase:
-    print "Processing %s" % letter
-    test = Match.objects.filter(i1__last_name__startswith='BANKS', for_training=False)
+nameid = 0
+first_letter_last_ln = ''
+for ln in Contribution.objects.all().values('last_name').distinct():
+    ln = ln['last_name']
+
+    if ln[0] <> first_letter_last_ln:
+        print ln[0]
+
+    test = Match.objects.filter(c1__last_name=ln, for_training=False)
+
+    # If len(test) == 1: Assign its own ID. Else:
 
     clf = DecisionTreeClassifier(compute_importances=True)
     clf = clf.fit([eval(t.features) for t in training], training_correct)
@@ -19,12 +27,14 @@ for letter in string.uppercase:
     for t in test:
         m = clf.predict_proba(eval(t.features))
         if m[0][1] > 0.0:
-            G.add_edge(t.i1, t.i2)
+            G.add_edge(t.c1, t.c2)
 
     ccs = connected_components(G)
 
     for c in enumerate(ccs):
         did = c[0]
         for i in c[1]:
-            contribs = Contribution.objects.filter(contributor_name=i.contributor_name, city=i.city, state=i.state, employer=i.employer, occupation=i.occupation)
-            contribs.update(donor_id=did)
+            i.donor_id=int('%s%s' % (nameid, did))
+            i.save()
+    nameid += 1
+    first_letter_last_ln = ln[0]
